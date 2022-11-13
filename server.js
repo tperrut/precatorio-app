9cc574ccbc94d783ccc95bfb606e286d97084c26
Thiago Perrut <thi.perrut@gmail.com>
2022-11-12 22:08:30 -0300

    teste deploy 6 -> add node server.js

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const app = express()
+const {resolve} = require('path')
+
+
 const port = 3000
 
 
+app.use('/',
+    express.static(
+        resolve(__dirname, './build')
+    )
+)
 
 app.listen(process.env.PORT || port, (err) => {
     if(err){
         console.log(`Error: ${err}`)     
     }
 
-   console.log(`Example app listening on port ${port}`)
+   console.log(`Precatorio app listening on port ${port}`)
 })
